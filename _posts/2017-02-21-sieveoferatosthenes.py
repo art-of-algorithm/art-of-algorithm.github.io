1fbf1bfdcae8ecbed63c4418fe011b9b1016a0e3
Rahul <rahulrock0082@gmail.com>
2017-02-21 10:12:34 +0530

    Update and rename sieveoferatosthenes.py to 2017-02-21-sieveoferatosthenes.py

--- _posts/2017-02-21-sieveoferatosthenes.py
+++ _posts/2017-02-21-sieveoferatosthenes.py
@@ -1,9 +1,11 @@
-impport math
+ #for more reading and detail https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes"
+#importing Math function
+import math
 n=int(raw_input())
 a=[True]*n
 a[0]=False
 a[1]=False
-for i in range(2,math.sqrt(n)):
+for i in range(2,int(math.sqrt(n))):
     if a[i] is True:
         for j in range(i**2,n,i):
             a[j]=False
